441b7437e635396526bc3aa628b156190e1e9f10
Simone Mauro <simon.mauro@virgilio.it>
2022-06-27 14:26:16 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+int somma = 0; 
+
+for (int i = 0; i < 10; i++){
+    Console.WriteLine("inserisci un numero");
+    int numero = Convert.ToInt32(Console.ReadLine());
+    somma += numero;
+}
+
+Console.WriteLine(somma);
