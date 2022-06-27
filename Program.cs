05d741c8e3ab449b678d3ae9b25d597c7e44a972
Simone Mauro <simon.mauro@virgilio.it>
2022-06-27 14:38:23 +0200

    snack 4 completato

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
 ﻿
+
+//SNACK 4, somma e media dei numeri da 2 a 10
+int[] listaNumeri = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int sommaNumeri = 0;
+int mediaNumeri = 0;
+
+for(int i = 0; i < listaNumeri.Length; i++){
+    sommaNumeri += listaNumeri[i];
+    mediaNumeri = sommaNumeri / listaNumeri.Length;
+    
+}
+
+Console.WriteLine($"La somma dei numeri da 2 a 10 è {sommaNumeri} mentre la media è {mediaNumeri}");
+
+
+
+return;
+
+//SNACK 3, somma di 10 numeri chiesti dall'utente
 int somma = 0; 
 
 for (int i = 0; i < 10; i++){
